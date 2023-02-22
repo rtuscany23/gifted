c7ab52d259b0a00504291523ed3d2b0b9eb9f6bc
Rich Tuscany <rtuscany23@gmail.com>
2023-02-21 17:45:54 -0700

    did more reading than coding today

---
+++ src/components/GiftCard.vue
@@ -0,0 +1,25 @@
+<template>
+<img :src="gift.url" :alt="gift.tag" class="img-fluid rounded selectable">
+  <h2>
+    {{ gift.tag }}
+  </h2>
+</template>
+
+
+<script>
+import { Gift } from '../models/Gift.js';
+
+// export default {
+//   // NOTE props allow us to pass down data from a parent component
+//   props: {
+//     movie: { type: Movie }
+//   },
+//   setup() {
+//     return {};
+//   },
+// }
+
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      <form @submit.prevent="">
+          <div class="input-group mb-3 me-3">
+            <input v-model="editable.query" required type="text" class="form-control" placeholder="New Gift"
+              aria-label="New Gift" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
+              <i class="">!</i>
+            </button>
+          </div>
+        </form>
+    </div>
+  </div>
+  <div class="row">
+          <div v-for="giftForTheVFor in gifts" key: giftForTheVFor.id class="col-md-3 p-4">
+
+      </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+import { AppState } from "../AppState.js";
+
+
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
